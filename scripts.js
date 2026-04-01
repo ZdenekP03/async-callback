3b37c8f2dc3f09ea7985fb185f442b76c4f8b352
Ujo <z.peconka@gmail.com>
2026-04-01 15:47:43 +0200

    runtime implementation

--- scripts.js
+++ scripts.js
@@ -5,29 +5,30 @@ const r = new rive.Rive({
     src: "async_callback_2903_export.riv",
     canvas: document.getElementById("rive-canvas1"),
     autoplay: true,
+    autoBind: true,
     layout: new rive.Layout({
         fit: rive.Fit.contain,
         alignment: rive.Alignment.Center,
     }),
     artboard: "Artboard-main", // Optional. If not supplied the default is selected
-    stateMachines: "StateMachine",
+    stateMachines: "State Machine 1",
     onLoad: () => {
         r.resizeDrawingSurfaceToCanvas();
 
-        const artboard = r.activeArtboard;
-        const viewModel = artboard.viewModel('ViewModel1');
-        viewModel.step = 0;
+        //const artboard = r.activeArtboard;
+        const viewModelInstance = r.viewModelInstance;
+        viewModelInstance.number('step').value = 0;
 
         clickArea.addEventListener("click", () => {
-            if (viewModel && viewModel.step < 13) {
-                viewModel.step++;
+            if (viewModelInstance.number('step').value < 13) {
+                viewModelInstance.number('step').value++;
             }
         });
 
         clickArea.addEventListener("contextmenu", (event) => {
             event.preventDefault();
-            if (viewModel && viewModel.step > 0) {
-                viewModel.step--;
+            if (viewModelInstance.number('step').value > 0) {
+                viewModelInstance.number('step').value--;
             }
         });
     }
@@ -36,5 +37,5 @@ const r = new rive.Rive({
 
 window.addEventListener('resize', ()=>{
         r.resizeDrawingSurfaceToCanvas();
-    })
+})
 
